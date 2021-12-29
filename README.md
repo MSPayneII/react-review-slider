f207a32392008875d3b0e83abd438a54703035c0
Michael Payne <mpaynei@umich.edu>
2021-12-28 22:40:19 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center">Welcome to my React Review Slider Project 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+  <img alt="Netlify Status" src="https://api.netlify.com/api/v1/badges/501cc357-6d83-4a98-ba10-6996b005a96c/deploy-status" />
+</p>
+
+## Reason for choosing this project:
+
+This was a hands-on React project provided by the John Smilga as part of his Udemy React Tutorial and Projects Course. This project was designed to help students become familiar with the useState, useEffect, and React in general.
+
+### 🏠 [Homepage](https://mpayne-react-experience.netlify.app/)
+
+## Functionality:
+
+- The application serves as slider for displaying reviews/testimonials. The slider is timed to autorotate every 6 seconds. Additionally, a user can press either a "prev" or "next" button to cycle through the reviews at their own pace.
+
+## Technologies:
+
+- React
+
+## Author
+
+👤 **Michael Payne**
+
+- Website: (https://michaelspayneii.com)
+- Github: [@MSPayneII](https://github.com/MSPayneII)
+- LinkedIn: [@michaelspayneii](https://linkedin.com/in/michaelspayneii)
---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+
+function App() {
+  const [reviews, setReviews] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = reviews.length - 1;
+
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [reviews, index]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 6000);
+
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <main>
+      <section className="section">
+        <div className="title">
+          <h2>
+            <span>/</span> Reviews
+          </h2>
+        </div>
+      </section>
+
+      <section className="section-center">
+        {reviews.map((review, reviewIndex) => {
+          const { id, image, name, title, quote } = review;
+
+          let position = "nextSlide";
+
+          if (reviewIndex === index) {
+            position = "activeSlide";
+          }
+
+          if (
+            reviewIndex === index - 1 ||
+            (index === 0 && reviewIndex === reviews.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
